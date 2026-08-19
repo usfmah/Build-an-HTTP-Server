f862d4cf4456996f8d385ca28852cbfe4b04807c
Soofa <youssefjoo2255@gmail.com>
2026-08-20 01:52:36 +0300

    refactoring the first 2 stages

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,8 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.end("HTTP/1.1 200 OK\r\n\r\n");
+
 });
 
 server.listen(4221, "localhost");
