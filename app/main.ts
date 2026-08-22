dc5cc11d84c175f0c826cd3dd19890179ad5d63d
Soofa <youssefjoo2255@gmail.com>
2026-08-22 23:24:45 +0300

    Extracting the URL Path

--- app/main.ts
+++ app/main.ts
@@ -17,11 +17,12 @@ const server = net.createServer((socket) => {
     const URL = parts[1];
     console.log(URL)
 
-    if (URL == '/') {
+    if (URL === '/') {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+    socket.end();
   })
 
   
